4a3f97985a6953112a4f8869ab399f88f56370b0
DRlFTER <wwwkaweesha5@gmail.com>
2025-08-13 20:03:32 +0530

    Initiated the student dashboard

---
+++ app/views/common/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title><?= isset($pageTitle) ? $pageTitle : 'UCSC Help Desk'; ?></title>
+  <link rel="stylesheet" href="/app/views/student/dashboard/dashboard.css" />
+  <link rel="stylesheet" href="/app/views/common/css/components.css" />
+</head>
+<body>
---
+++ app/views/common/navbar.php
@@ -0,0 +1,31 @@
+<header>
+  <nav class="navbar">
+    <div class="navbarLeft">
+      <img src="/public/img/logo.svg" alt="UCSC Logo" class="logo" />
+      <span class="navbarBrand">UCSC Help Desk</span>
+    </div>
+    <div class="navbarCenter">
+      <div class="navbarMenuContainer">
+        <a href="#" class="navbarLink active">Dashboard</a>
+        <a href="#">Calendar</a>
+        <a href="#">Tickets</a>
+        <a href="#">Forum</a>
+        <div class="btnHolder">
+          <button class="navbarNewTicket btnWSvg btnPrimaryText">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 30 30" fill="none">
+              <path d="M13.75 16.25H6.25V13.75H13.75V6.25H16.25V13.75H23.75V16.25H16.25V23.75H13.75V16.25Z" fill="#FEF7FF"/>
+            </svg>
+            <span>New Ticket</span>
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="navbarRight">
+      <div class="icon notification"></div>
+      <div class="profile">
+        <img src="/public/img/profile.svg" alt="Profile" />
+        <span>Profile Name</span>
+      </div>
+    </div>
+  </nav>
+</header>
