3f3b8553a47908be427edde6ac32c3c07f320295
DRlFTER <wwwkaweesha5@gmail.com>
2025-08-31 03:08:29 +0530

    feat(common): made the navbar universal, added a loading animation between pages

--- app/controllers/admin/Admin.php
+++ app/controllers/admin/Admin.php
@@ -6,7 +6,6 @@ class Admin extends Controller
     {
         $this->requireLogin('admin');
         $headContent = '
-        <link rel="stylesheet" href="/css/global/components.css" />
         <link rel="stylesheet" href="/css/admin/adminDashboard.css"/>';
          $this->view('adminDashboard', ['title' => 'Admin Dashboard', 'head' => $headContent]);
     }
