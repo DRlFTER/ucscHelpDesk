b212b155ed86d07c828d186b6d23c7f66998802c
BrianShanuka <brianshanuka@gmail.com>
2025-08-30 23:28:28 +0530

    created student new ticket view

--- app/views/student/ticketStudent.view.php
+++ app/views/student/ticketStudent.view.php
@@ -81,7 +81,8 @@ include_once(__DIR__ . "/../../views/common/navbar.php");
 
 					<div class="btnHolder">
 						<button class="btnPrimary btnPrimaryText" type="submit">
-							➤ Submit Ticket
+							<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M120-160v-240l320-80-320-80v-240l760 320-760 320Z"/></svg>
+                            Submit Ticket
 						</button>
 					</div>
 				</form>
