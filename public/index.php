252b74811574e2d8f86f2ebd21b1ce5d37a2972e
DRlFTER <wwwkaweesha5@gmail.com>
2025-09-01 14:01:57 +0530

    feat(auth): added a session time out, implemented proper login error msg displays

--- public/index.php
+++ public/index.php
@@ -1,8 +1,21 @@
 <?php
 
 session_start();
-
 require '../app/core/init.php';
+
+$timeout = 10;
+$currentPath = $_SERVER['REQUEST_URI'];
+
+if (!empty($_SESSION['user'])) {
+    if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity']) > $timeout) {
+        session_unset();
+        session_destroy();
+        header("Location: " . ROOT . "login?expired=1");
+        exit;
+    }
+
+    $_SESSION['last_activity'] = time();
+}
 
 DEBUG ? ini_set('display_errors', 1) : ini_set('display_errors', 0);
 $app = new App;
