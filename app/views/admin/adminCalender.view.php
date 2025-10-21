50add7a3e9e4da66ce1e595148cccbf058abd497
DRlFTER <wwwkaweesha5@gmail.com>
2025-10-21 17:11:20 +0530

    feat(admin): calender ui done

--- app/views/admin/adminCalender.view.php
+++ app/views/admin/adminCalender.view.php
@@ -11,40 +11,28 @@
                     <h2>Monday</h2>
                 </div>
                 <div class="todayEvents">
-                    <div class="noEvents">No events for today</div>
                     <div class="event">
-                        <h2>Event Title</h2>
+                        <h2>Team sync</h2>
+                        <p>All day</p>
+                    </div>
+                    <div class="event">
+                        <h2>Ticket review</h2>
                         <p>All day</p>
                     </div>
                 </div>
                 <div class="addEvent">
-                    <div class="inputEvent">
-                        <input type="text" id="eventTitle" placeholder="Event Title" />
-                         <div class="btnHolder">
+                        <input type="text" class="inputEvent" id="eventTitle" placeholder="Add event" />
                            <button class="btnSvg">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="25"
-                height="25"
-                viewBox="0 0 25 25"
-                fill="none"
-              >
-                <path
-                  d="M3.5 20.2793V14.2793L11.5 12.2793L3.5 10.2793V4.2793L22.5 12.2793L3.5 20.2793Z"
-                  fill="white"
-                />
-              </svg>
+             <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="36px" fill="#ffffffff"><path d="M433-433H222q-19.75 0-33.37-13.68Q175-460.35 175-480.18q0-19.82 13.63-33.32Q202.25-527 222-527h211v-211q0-19.63 13.68-33.81Q460.35-786 480.18-786q19.82 0 33.32 14.19Q527-757.63 527-738v211h211q19.63 0 33.81 13.68Q786-499.65 786-479.82q0 19.82-14.19 33.32Q757.63-433 738-433H527v211q0 19.75-13.68 33.37Q499.65-175 479.82-175q-19.82 0-33.32-13.63Q433-202.25 433-222v-211Z"/></svg>
             </button>
-                        </div>
-                    </div>
                 </div>
             </div>
             <div class="settingsRight">
                 <div class="calendarContainer">
                     <div class="calendarHeader">
-                        <button id="prevMonthBtn" class="btnSecondary"><span class="btnSecondaryText">&lt;</span></button>
+                        <button id="prevMonthBtn" class="btnSecondary" style="background: none; "><span class="btnSecondaryText">&lt;</span></button>
                         <h2 id="monthYear"></h2>
-                        <button id="nextMonthBtn" class="btnSecondary"><span class="btnSecondaryText">&gt;</span></button>
+                        <button id="nextMonthBtn" class="btnSecondary" style="background: none;"><span class="btnSecondaryText">&gt;</span></button>
                     </div>
                     <div class="calendarGrid">
                         <div class="calendarWeekdays">
