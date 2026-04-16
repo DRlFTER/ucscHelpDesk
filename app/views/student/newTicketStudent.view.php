6106465dfc9c05beb364d187e2354c5923e7094e
BrianShanuka <brianshanuka@gmail.com>
2026-04-16 18:53:54 +0530

    changes ig

--- app/views/student/newTicketStudent.view.php
+++ app/views/student/newTicketStudent.view.php
@@ -136,7 +136,7 @@ include_once(__DIR__ . "/../../views/common/navbar.php");
 					<h3>Knowledge Base</h3>
 					<div class="kbSearch">
 						<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#6b7280" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"/><line x1="21" y1="21" x2="16.65" y2="16.65"/></svg>
-            <input type="text" placeholder="Search FAQs, forums, and help articles..." onclick="event.stopPropagation();" onfocus="event.stopPropagation();" />
+            <input type="text" placeholder="Search FAQs, forums, and help articles..." />
 					</div>
 				</section>
 
